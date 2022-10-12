2d610254632e82e47f143d6430e891f4a284f7d3
Ricardo Arredondo <ricardo.richo@gmail.com>
2022-10-12 08:01:43 -0500

    Add ace-link-dashboard-remove to remove item under.

--- ace-link-dashboard.el
+++ ace-link-dashboard.el
@@ -15,9 +15,23 @@
                (avy--style-fn avy-style)))))
     (ace-link--dashboard-action pt)))
 
+;;;###autoload
+(defun ace-link-dashboard-remove ()
+  "Call remove action on widget."
+  (interactive)
+  (let ((point (avy-with 'ace-link-dashboard-remove
+                 (avy-process
+                  (mapcar #'cdr (ace-link--dashboard-collect))
+                  (avy--style-fn avy-style)))))
+    (ace-link--dashboard-remove point)))
+
 (defun ace-link--dashboard-action (point)
   "Call action at POINT when widget is selected."
   (funcall 'widget-button-press point))
+
+(defun ace-link--dashboard-remove (point)
+  "Call remove action on item at POINT."
+  (dashboard-remove-item-under))
 
 (defun ace-link--dashboard-collect ()
   "Collect all widgets in the current `dashboard-mode' buffer."
