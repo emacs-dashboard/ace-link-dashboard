cf79055449441a8da3c380a32670775444913da5
Ricardo Arredondo <839303+ricardoricho@users.noreply.github.com>
2023-04-18 15:36:54 -0600

    Add dummy test. (#4)
    * Add dummy test.
    
    * Mark snapashot emacs version as experimental.

---
+++ test/ace-link-dashboard-tests.el
@@ -0,0 +1,10 @@
+;;; ace-link-dashboard-tests --- Test for ace-link-dashboard
+;;; Commentary:
+;;; Code:
+
+(ert-deftest ace-link-dashboard--widget-point-test ()
+  "Widget point."
+  (should "pass"))
+
+(provide  'ace-link-dashboard-tests)
+;;; ace-link-dashboard-tests.el ends here
